cea0add182906974eb13d5cca96a52db8464ba74
kishore99 <bhashyamyk@gmail.com>
2018-11-15 12:14:33 +0530

    Updated

---
+++ BakingCorner/app/build/generated/source/buildConfig/debug/com/example/kishore_yuva/bakingcorner/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.kishore_yuva.bakingcorner;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.kishore_yuva.bakingcorner";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from default config.
+  public static final String API_BASE_URL = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/";
+}
